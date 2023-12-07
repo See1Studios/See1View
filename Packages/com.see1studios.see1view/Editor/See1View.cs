287e376c342fc89e48cdc65021c9e44420e9135f
Jongwoo Park <see1studios@gmail.com>
2023-12-08 01:30:08 +0900

    Update See1View.cs

--- Packages/com.see1studios.see1view/Editor/See1View.cs
+++ Packages/com.see1studios.see1view/Editor/See1View.cs
@@ -4367,18 +4367,18 @@ where T : IEquatable<T>
                             AnimationMode.SampleAnimationClip(animated.instance, _currentClip, (float)time);
                             foreach (var modifier in modifierList)
                             {
-                                var clip = new AnimationClip();
-                                Keyframe[] keys;
-                                keys = new Keyframe[3];
-                                keys[0] = new Keyframe(0.0f, 0.0f);
-                                keys[1] = new Keyframe(1.0f, 1.5f);
-                                keys[2] = new Keyframe(2.0f, 0.0f);
-                                var curve = new AnimationCurve(keys);
-                                clip.SetCurve("", typeof(Transform), "localPosition.x", curve);
-                                // update the clip to a change the red color
-                                curve = AnimationCurve.Linear(0.0f, 1.0f, 2.0f, 0.0f);
-                                clip.SetCurve("", typeof(Material), "_Color.r", curve);
-                                AnimationUtility.SetAdditiveReferencePose(_currentClip, clip, (float)time);
+                                //var clip = new AnimationClip();
+                                //Keyframe[] keys;
+                                //keys = new Keyframe[3];
+                                //keys[0] = new Keyframe(0.0f, 0.0f);
+                                //keys[1] = new Keyframe(1.0f, 1.5f);
+                                //keys[2] = new Keyframe(2.0f, 0.0f);
+                                //var curve = new AnimationCurve(keys);
+                                //clip.SetCurve("", typeof(Transform), "localPosition.x", curve);
+                                //// update the clip to a change the red color
+                                //curve = AnimationCurve.Linear(0.0f, 1.0f, 2.0f, 0.0f);
+                                //clip.SetCurve("", typeof(Material), "_Color.r", curve);
+                                //AnimationUtility.SetAdditiveReferencePose(_currentClip, clip, (float)time);
                                 modifier.ApplyToCurrent();
                             }
                         }
